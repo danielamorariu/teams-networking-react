beb7d21fa52e446ddd04ffab2a6be462ac2d7e5e
Daniela Morariu <daniela.morariu@target-e.com>
2023-05-10 18:10:23 +0300

    Clean up old code

--- src/App.tsx
+++ src/App.tsx
@@ -127,20 +127,4 @@ function App() {
   );
 }
 
-function App_old() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={selfie} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
 export default App;
