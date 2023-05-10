6a8c6a87a1d448e0af3c4120665db6dc220d229f
Daniela Morariu <daniela.morariu@target-e.com>
2023-05-10 20:58:16 +0300

    Display array of teams

--- src/teams/TeamsTable.tsx
+++ src/teams/TeamsTable.tsx
@@ -1,5 +1,42 @@
 import "./style.css";
 export function TeamsTable() {
+  const teams = [
+    {
+      id: "toze8j1610313009673",
+      promotion: "html",
+      members: "Nicolae Matei, HTML",
+      name: "Web Presentation",
+      url: "https://github.com/nmatei/web-intro-presentation"
+    },
+    {
+      id: "ezabnf1630345987541",
+      promotion: "css",
+      members: "Nicolae",
+      name: "Names",
+      url: "https://github.com/nmatei/nmatei.github.io"
+    },
+    {
+      id: "86mq81630347385708",
+      promotion: "js",
+      members: "Matei, Andrei",
+      name: "JS/HTML/CSS Quiz",
+      url: "https://github.com/nmatei/simple-quiz-app"
+    },
+    {
+      id: "w2aal1630347411901",
+      promotion: "js",
+      members: "FastTrackIT Students, Nicolae",
+      name: "Teams Networking",
+      url: "https://github.com/nmatei/teams-networking"
+    },
+    {
+      id: "w2aal1630347411902",
+      promotion: "react",
+      members: "FastTrackIT Students, Nicolae",
+      name: "Teams Networking React",
+      url: "https://github.com/nmatei/teams-networking-react"
+    }
+  ];
   return (
     <form id="editForm" action="" method="post">
       <table>
@@ -23,7 +60,37 @@ export function TeamsTable() {
             <th></th>
           </tr>
         </thead>
-        <tbody></tbody>
+        <tbody>
+          {teams.map(({ id, url, promotion, members, name }) => {
+            let displayUrl = url;
+            if (url.startsWith("https://")) {
+              displayUrl = url.substring(8);
+            }
+            return (
+              <tr key={id}>
+                <td>
+                  <input type="checkbox" name="selected" value={id} />
+                </td>
+                <td>{promotion}</td>
+                <td>{members}</td>
+                <td>{name}</td>
+                <td>
+                  <a href={url} target="_blank" rel="noreferrer">
+                    {displayUrl}
+                  </a>
+                </td>
+                <td>
+                  <a data-id={id} className="link-btn remove-btn">
+                    ✖️
+                  </a>
+                  <a data-id={id} className="link-btn edit-btn">
+                    ✎
+                  </a>
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
         <tfoot>
           <tr>
             <td></td>
